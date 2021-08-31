75a60ce756a2af5b3d8672c5f00a782e0b551db1
Tanvi Nanda <tanvi789@gmail.com>
2021-08-31 22:37:25 +0100

    Added more features

--- tests/step_definitions/test_api_steps.py
+++ tests/step_definitions/test_api_steps.py
@@ -1,16 +1,21 @@
-from pytest_bdd import scenario, parsers, given, when, then
+from functools import partial
+from pytest_bdd import scenarios, parsers, given, when, then
 
 
 # Scenario is a decorator here
-@scenario('../features/cucumber_api_test.feature', 'to verify the api')
-def test_api():
-    pass
+# @scenario('../features/cucumber_api_test.feature', 'to verify the api')
+# def test_api():
+#     pass
+
+scenarios('../features/cucumber_api_test.feature')
 
 
 EXTRA_TYPES = {
     'Number': int,
 }
 
+# parse_num = partial(parsers.cfparse,extra_types=EXTRA_TYPES)
+# @when(parse_num('request is equal to "{initial:Number}"'))
 
 @given("API request")
 def api_request():
