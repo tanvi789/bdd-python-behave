dbaa1327a7deaa52079139456659865e892ed88a
Tanvi Nanda <tanvi789@gmail.com>
2021-08-31 23:22:17 +0100

    need to work on SC

--- tests/features/cucumber_api_test.feature
+++ tests/features/cucumber_api_test.feature
@@ -3,3 +3,14 @@ Feature: Api
   Scenario: to verify the api
     Given API request
     When request is equal to "2"
+
+  Scenario Outline: the api
+    Given number one "<initial>"
+    When request add "<some>"
+    Then total add "<sum_total>"
+
+    Examples: Amounts
+      | initial | some | sum_total |
+      | 2       | 4    | 6     |
+      | 0       | 0    | 0     |
+      | 1       | 1    | 2     |
--- tests/step_definitions/test_api_steps.py
+++ tests/step_definitions/test_api_steps.py
@@ -1,18 +1,27 @@
 from functools import partial
 from pytest_bdd import scenarios, parsers, given, when, then
 
-
 # Scenario is a decorator here
 # @scenario('../features/cucumber_api_test.feature', 'to verify the api')
 # def test_api():
 #     pass
 
-scenarios('../features/cucumber_api_test.feature')
-
+x = 0
+y = 0
 
 EXTRA_TYPES = {
     'Number': int,
 }
+
+CONVERTERS = {
+    'initial': int,
+    'some': int,
+    'sum_total': int
+}
+# scenarios('../features/cucumber_api_test.feature')
+#
+scenarios('../features/cucumber_api_test.feature', example_converters=CONVERTERS)
+
 
 # parse_num = partial(parsers.cfparse,extra_types=EXTRA_TYPES)
 # @when(parse_num('request is equal to "{initial:Number}"'))
@@ -25,3 +34,24 @@ def api_request():
 @when(parsers.cfparse('request is equal to "{initial:Number}"', extra_types=EXTRA_TYPES))
 def request_equal(initial):
     assert initial == initial
+
+
+@given('number one "<initial>"')
+def initial_value(initial):
+    y = initial
+    print(y)
+    # assert initial == initial
+
+
+@when('request add "<some>"')
+def step_impl(some):
+    print(y)
+    x = y + some
+    print(x)
+    # assert some == some
+
+
+@then('total add "<sum_total>"')
+def step_impl(sum_total):
+    assert x == sum_total
+    # assert sum_total == sum_total
